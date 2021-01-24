4fd105cc466d08c29d54525acf298b3150795316
zubearc <zubear@zubear.me>
2021-01-24 00:51:16 -0500

    add existing JHU data transformer, add submodules

---
+++ generator/CreateGlobalCovidData.py
@@ -0,0 +1,283 @@
+# This file was created March 2020
+import csv,json,math
+import time
+import GlobalCodes
+
+tsDays = []
+tsConfirmed = {}
+tsDeaths = {}
+tsRecovered = {}
+tsData = {}
+
+with open('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv', 'r') as f:
+    time_series_covid19_confirmed_global = f.readlines()
+
+    with open('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv', 'r') as g:
+        time_series_covid19_confirmed_US = g.readlines()
+        for line in time_series_covid19_confirmed_US:
+            if 'Puerto Rico' in line:
+                time_series_covid19_confirmed_global.append(',Puerto Rico,' + line.split('US,')[1].replace('"Puerto Rico, US",', ''))
+                # print '->',time_series_covid19_confirmed_global[-1]
+                # raise time_series_covid19_confirmed_global[-1]
+                # exit()
+
+    fl = time_series_covid19_confirmed_global.pop(0)
+    s = fl.strip().split(',')
+    for i in range(4, len(s)):
+        # print s[i]
+        tsDays.append(s[i])
+        tsConfirmed[s[i]] = {}
+
+with open('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv', 'r') as f:
+    time_series_covid19_deaths_global = f.readlines()
+    with open('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv', 'r') as g:
+        time_series_covid19_deaths_US = g.readlines()
+        for line in time_series_covid19_deaths_US:
+            if 'Puerto Rico' in line:
+                time_series_covid19_deaths_global.append(',Puerto Rico,' + line.split('US,')[1].replace('"Puerto Rico, US",2933408,', '').replace('.0,', ''))
+                print time_series_covid19_deaths_global[-1]
+
+    fl = time_series_covid19_deaths_global.pop(0)
+    s = fl.strip().split(',')
+    for i in range(4, len(s)):
+        # print s[i]
+        tsDeaths[s[i]] = {}
+
+with open('COVID-19/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv', 'r') as f:
+    time_series_covid19_recovered_global = f.readlines()
+    fl = time_series_covid19_recovered_global.pop(0)
+    s = fl.strip().split(',')
+    for i in range(4, len(s)):
+        # print s[i]
+        tsRecovered[s[i]] = {}
+
+
+# time.sleep(1)
+# print json.dumps(countyNameToIso3), len(countyNameToIso3.keys())
+# exit()
+
+countryNameToIso2 = GlobalCodes.countryNameToIso2
+countryNameToIso3 = GlobalCodes.countryNameToIso3
+countryPopulationsIso3 = GlobalCodes.countryPopulationsIso3
+
+# exit()
+
+r1 = csv.reader(time_series_covid19_confirmed_global)       #csv.reader is used to read a file
+for s in r1:
+    Province_State = s[0]
+    Country_Region = s[1]
+    Lat = s[2]
+    Long = s[3]
+
+    # if Country_Region == 'France':
+    #     print s
+    # print s
+
+    name = Province_State + ',' + Country_Region
+    for i in range(4, len(s)):
+        # print s[i]
+        x = tsDays[i - 4]
+        if x not in tsConfirmed:
+            tsConfirmed[x] = {}
+        
+        try:
+            iso2 = countryNameToIso2[name]
+            iso3 = countryNameToIso3[name]
+        except Exception:
+            # print 'did not find CC for ', name
+            iso2 = countryNameToIso2[Country_Region]
+            iso3 = countryNameToIso3[Country_Region]
+
+        # if Country_Region == 'France':
+        #     print iso2, iso3
+
+        tsConfirmed[x][iso2] = s[i]
+
+        value = int(s[i])
+
+        if x not in tsData:
+            tsData[x] = {}
+        if iso2 not in tsData[x]:
+            tsData[x][iso2] = [0,0,0 , 0, 0, 0]
+
+        # print x, Country_Region, value
+        tsData[x][iso2][0] += value
+
+        newval = tsData[x][iso2][0]
+
+        try:
+            _pop = countryPopulationsIso3[iso3]
+            pop = int(_pop)
+        except Exception:
+            print 'No pop for', Country_Region, iso3
+            pop = 1
+
+        per100k = math.ceil(((float(newval) / float(pop)) * 1000000) * 100)
+        # print per100k, newval, '/', pop
+        tsData[x][iso2][3 + 0] = per100k
+        
+
+r1 = csv.reader(time_series_covid19_deaths_global)       #csv.reader is used to read a file
+for s in r1:
+    Province_State = s[0]
+    Country_Region = s[1]
+    Lat = s[2]
+    Long = s[3]
+    
+    name = Province_State + ',' + Country_Region
+    # print s
+
+    for i in range(4, len(s)):
+        # print s[i]
+        x = tsDays[i - 4]
+        if x not in tsDeaths:
+            tsDeaths[x] = {}
+        
+        try:
+            iso2 = countryNameToIso2[name]
+            iso3 = countryNameToIso3[name]
+        except Exception:
+            # print 'did not find CC for ', name
+            iso2 = countryNameToIso2[Country_Region]
+            iso3 = countryNameToIso3[Country_Region]
+
+        tsDeaths[x][iso2] = s[i]
+
+        value = int(s[i])
+
+        if x not in tsData:
+            tsData[x] = {}
+        if iso2 not in tsData[x]:
+            tsData[x][iso2] = [0,0,0 , 0, 0, 0]
+
+        # print x, Country_Region, value
+        tsData[x][iso2][1] += value
+
+        newval = tsData[x][iso2][1]
+
+        try:
+            _pop = countryPopulationsIso3[iso3]
+            pop = int(_pop)
+        except Exception:
+            print 'No pop for', Country_Region, iso3
+            pop = 1
+
+        per100k = math.ceil(((float(newval) / float(pop)) * 1000000) * 100)
+        tsData[x][iso2][3 + 1] = per100k
+
+r1 = csv.reader(time_series_covid19_recovered_global)       #csv.reader is used to read a file
+for s in r1:
+    Province_State = s[0]
+    Country_Region = s[1]
+    Lat = s[2]
+    Long = s[3]
+
+    name = Province_State + ',' + Country_Region
+
+    for i in range(4, len(s)):
+        # print s[i]
+        x = tsDays[i - 4]
+        if x not in tsRecovered:
+            tsRecovered[x] = {}
+        
+        try:
+            iso2 = countryNameToIso2[name]
+            iso3 = countryNameToIso3[name]
+        except Exception:
+            # print 'did not find CC for ', name
+            iso2 = countryNameToIso2[Country_Region]
+            iso3 = countryNameToIso3[Country_Region]
+
+        tsRecovered[x][iso2] = s[i]
+
+        value = int(s[i])
+
+        if x not in tsData:
+            tsData[x] = {}
+        if iso2 not in tsData[x]:
+            tsData[x][iso2] = [0,0,0 , 0, 0, 0]
+
+        # print x, Country_Region, value
+        tsData[x][iso2][2] += value
+
+        newval = tsData[x][iso2][2]
+
+        try:
+            _pop = countryPopulationsIso3[iso3]
+            pop = int(_pop)
+        except Exception:
+            print 'No pop for', Country_Region, iso3
+            pop = 1
+
+        per100k = math.ceil(((float(newval) / float(pop)) * 1000000) * 100)
+        tsData[x][iso2][3 + 2] = per100k
+
+# for line in time_series_covid19_recovered_global:
+#     s = line.split(',')
+#     Province_State = s[0]
+#     Country_Region = s[1]
+#     Lat = s[2]
+#     Long = s[3]
+#     for i in range(4, len(s)):
+#         # print s[i]
+#         tsRecovered[tsDays[4 - i]] = s[i]
+
+# print tsConfirmed
+
+for x in tsData:
+    globalConfirmed = 0
+
+    allConfirmed = []
+    for iso2 in tsData[x]:
+        if iso2 == 'WX' or iso2 == 'WW' or iso2 == 'WY':
+            continue
+            # print iso2
+        v = tsData[x][iso2]
+        allConfirmed.append([ v[0], iso2 ])
+        globalConfirmed += v[0]
+    allConfirmed.sort()
+    allConfirmed.reverse()
+    tsData[x]['WX'] = allConfirmed[0:20]
+    tsData[x]['WW'] = [ globalConfirmed, 0, 0 ]
+
+
+for x in tsData:
+    allDeaths = []
+    globalDeaths = 0
+    for iso2 in tsData[x]:
+        if iso2 == 'WX' or iso2 == 'WW' or iso2 == 'WY':
+            continue
+            # print iso2
+        v = tsData[x][iso2]
+        allDeaths.append([ v[1], iso2 ])
+        globalDeaths += v[1]
+    allDeaths.sort()
+    allDeaths.reverse()
+    tsData[x]['WY'] = allDeaths[0:20]
+    tsData[x]['WW'][1] = globalDeaths
+
+for x in tsData:
+    allRecovered = []
+    globalRecoveries = 0
+
+    for iso2 in tsData[x]:
+        if iso2 == 'WX' or iso2 == 'WW' or iso2 == 'WY':
+            continue
+            # print iso2
+        v = tsData[x][iso2]
+        allRecovered.append([ v[2], iso2 ])
+        # print iso2, v
+        globalRecoveries += v[2]
+    allRecovered.sort()
+    allRecovered.reverse()
+    tsData[x]['WZ'] = allRecovered[0:20]
+    tsData[x]['WW'][2] = globalRecoveries
+
+reformatted = []
+for day in tsDays:
+    dfd = tsData[day]
+    reformatted.append(dfd)
+
+with open('../data/global/country-daily.json', 'w') as f:
+    # f.write(json.dumps(tsData))
+    f.write(json.dumps({ 'daily': reformatted, 'days': tsDays }))
---
+++ generator/GlobalCodes.py
@@ -0,0 +1,102 @@
+import csv, json
+
+countryNameToIso2 = {}
+countryNameToIso3 = {}
+Iso3toIso2 = {}
+Iso2ToName = {}
+# YD= Diamond Princess, 3K people onboard estimate
+# all other pops from worldometers.info estimates (from UN) or CIA projections
+countryPopulationsIso3 = {'TWN': 23806748, 'NLD': 17126049, 'BES': 25157, 'MSR': 5900, 'AIA': 15094, 'MTQ': 376480,
+                          'YD': 3000, 'YE': 1200,
+                          'ERI': 3534851, 'VAT': 618, 'XKS': 1932774, 'GUF': 290691, 'GLP': 395700, 'MYT': 270372, 'REU': 859959, 'BLM': 9131 }
+
+with open('COVID-19/csse_covid_19_data/UID_ISO_FIPS_LookUp_Table.csv', 'r') as f:
+    UID_ISO_FIPS_LookUp_Table = f.readlines()
+
+UID_ISO_FIPS_LookUp_Table.pop(0)
+# csv.reader is used to read a file
+reader = csv.reader(UID_ISO_FIPS_LookUp_Table)
+first = True
+for row in reader:
+    UID, iso2, iso3, code3, FIPS, Admin2, Province_State, Country_Region, Lat, Long_, Combined_Key, pop = row
+
+    # UPDATE: Fixed this properly!  
+    # Fix China Special Administrative Regions (SAR) into their own psuedo-countries
+    # if Country_Region == 'China' and Province_State == 'Hong Kong':
+    #     Country_Region = 'Hong Kong'
+    # if Country_Region == 'China' and Province_State == 'Macau':
+    #     Country_Region = 'Macau'
+    # if Country_Region == 'France':
+    #     continue
+
+        # print row, iso2, iso3
+    name = Province_State + ',' + Country_Region
+    countryNameToIso2[name] = iso2
+    countryNameToIso3[name] = iso3
+    Iso3toIso2[iso3] = iso2
+    Iso2ToName[iso2] = name
+    countryPopulationsIso3[iso3] = pop
+    # print Country_Region, iso3, len(countyNameToIso3.keys())
+
+countryNameToIso3[',Diamond Princess'] = 'YD'
+countryNameToIso3[',MS Zaandam'] = 'YE'
+
+# print countryNameToIso2
+
+with open('global/global-country-code-map.tsv', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+        _name,iso2,iso3,num = line.strip().split('\t')
+        # if name =='China':
+        #     print line
+        #     print countryNameToIso2['China']
+        if _name not in countryNameToIso2:
+            try:
+                name, _t = _name.replace(')', '').split('(')
+                name = name.strip()
+            except Exception:
+                name = _name
+            countryNameToIso2[name] = iso2
+            countryNameToIso3[name] = iso3
+            Iso3toIso2[iso3] = iso2
+            if '(' in name:
+                print name
+                _namef = _t.title() + ' ' + name
+                Iso2ToName[iso2] = _namef
+            else:
+                Iso2ToName[iso2] = name
+# print countryNameToIso2
+with open('global/global-population-worldbank.csv', 'r') as f:
+    lines = f.readlines()
+    lines.pop(0)
+    reader=csv.reader(lines)       #csv.reader is used to read a file
+    first = True
+    for row in reader:
+        # "Country Name","Country Code","1960","1961","1962","1963","1964","1965","1966","1967","1968","1969","1970","1971","1972","1973","1974","1975","1976","1977","1978","1979","1980","1981","1982","1983","1984","1985","1986","1987","1988","1989","1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019",
+        iso3 = row[1]
+        pop18 = row[len(row) - 3]
+        # print row[len(row) - 3]
+        if pop18:
+            countryPopulationsIso3[iso3] = pop18
+        # countyNameToIso2[Country_Region] = iso2
+
+# print countryNameToIso3
+# print countryPopulationsIso3
+
+# print Iso3toIso2
+
+# assert countryNameToIso2['China'] == 'CN'
+
+print json.dumps(Iso2ToName)
+
+iso2p = {}
+for iso3cc in countryPopulationsIso3:
+    try:
+        d = countryPopulationsIso3[iso3cc]
+        print d
+        iso2 = Iso3toIso2[iso3cc]
+        iso2p[iso2] = int(d)
+    except Exception:
+        print 'fail', iso3cc
+
+print json.dumps(iso2p)
